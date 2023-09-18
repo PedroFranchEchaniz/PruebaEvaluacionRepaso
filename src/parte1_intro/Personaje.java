874db0e740a46c4783a7752e9246ce2174f28882
Pedro Franch Echaniz <pedrofe.av@gmail.com>
2023-09-18 20:33:41 +0200

    agregados metodos recibirgolpe y curacion

--- src/parte1_intro/Personaje.java
+++ src/parte1_intro/Personaje.java
@@ -1,5 +1,116 @@
 package parte1_intro;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Personaje {
 
+	private String name;
+	private int life;
+	private int score;
+	private String[] mochila = new String[10];
+
+	public Personaje() {
+		super();
+	}
+
+	public Personaje(String name, String[] mochila) {
+		super();
+		this.name = name;
+		this.life = 100;
+		this.score = 0;
+		this.mochila = mochila;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getLife() {
+		return life;
+	}
+
+	public void setLife(int life) {
+		this.life = life;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public String[] getMochila() {
+		return mochila;
+	}
+
+	public void setMochila(String[] mochila) {
+		this.mochila = mochila;
+	}
+
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(mochila);
+		result = prime * result + Objects.hash(life, name, score);
+		return result;
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Personaje other = (Personaje) obj;
+		return life == other.life && Arrays.equals(mochila, other.mochila) && Objects.equals(name, other.name)
+				&& score == other.score;
+	}
+
+	public String toString() {
+		return "Personaje [name=" + name + ", life=" + life + ", score=" + score + ", mochila="
+				+ Arrays.toString(mochila) + "]";
+	}
+
+	public void agregar(String agregado) {
+		for (String m : mochila) {
+			if (m.isEmpty()) {
+				m = agregado;
+				System.out.println("Tiene " + m.length() + " objetos en el inventario");
+
+			} else {
+				System.out.println("No tiene espacio en la mochila");
+			}
+		}
+
+	}
+	
+	public void puntosDeVida(int golpe) {
+		life -= golpe;
+		if(life>=0) {
+			System.out.println("Tiene " +life +" puntos de vida");
+		}else {
+			life = 0;
+			System.out.println("Tiene " +life +" puntos de vida");
+		}
+	}
+	
+	public void regenerarVida() {
+		for (String m : mochila) {
+			if(m.equalsIgnoreCase("poción")) {
+				life+=10;
+				System.out.println("botiquín aplicado +10");
+				System.out.println("Tiene " +life +" puntos de vida");
+			}else {
+				System.out.println("No tiene poción");
+			}
+		}
+	}
 }
